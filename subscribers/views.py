49a2a97949fd7d33ea89b2bddfa742d5cc498520
devpotato_100 <92353593+Jinwoo-Ha@users.noreply.github.com>
2026-02-15 00:03:54 +0000

    구독 완료 팝업 추가

--- subscribers/views.py
+++ subscribers/views.py
@@ -10,10 +10,10 @@ def subscribe(request):
         email = request.POST.get('email')
         if email:
             if Subscriber.objects.filter(email=email).exists():
-                messages.warning(request, '이미 구독 중인 이메일입니다.')
+                messages.warning(request, '이미 구독한 이메일이에요!')
             else:
                 Subscriber.objects.create(email=email)
-                messages.success(request, '구독해주셔서 감사합니다!')
+                messages.success(request, '구독이 완료되었습니다!')
         else:
             messages.error(request, '유효한 이메일 주소를 입력해주세요.')
     return redirect('home')
